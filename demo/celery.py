59960cb9e732cd40007a4503bcf8b53ff56d5fe7
a.combeau <a.combeau@unistra.fr>
2019-06-06 14:07:26 +0200

    Adde demo

---
+++ demo/celery.py
@@ -0,0 +1,71 @@
+"""Celery worker base"""
+
+from celery import Celery
+import sentry_sdk as sentry
+
+# sentry integration for classic tasks
+from sentry_sdk.integrations.celery import CeleryIntegration
+
+# custom customer class
+from skinos.custom_consumer import CustomConsumer
+
+CELERY_BROKER = "amqp://{user}:{password}@{host}:{port}{vhost}".format(
+    user='test',
+    password='test',
+    host='127.0.0.1',
+    port='5672',
+    vhost='/test'
+)
+BROKER_TRANSPORT_OPTIONS = {'confirm_public': True}
+
+# Celery init
+app = Celery(
+    'test',
+    backend='',
+    broker=CELERY_BROKER,
+    include=[]
+)
+app.config_from_object(BROKER_TRANSPORT_OPTIONS)
+
+# Init sentry
+sentry.init(
+    "my_projet_key",
+    integrations=[CeleryIntegration()]
+)
+
+############################################
+#################  SKINOS  #################
+############################################
+
+# set sentry to True and set raise to False (i.e: if error occur, error is not re-raise, but ignored)
+# if you don't use it, default values are False and False
+CustomConsumer.with_sentry(False, False)
+
+# defined a new exchange with a name and a binding key (always a topic)
+CustomConsumer.add_exchange('test', "test.*.*")
+
+
+# Define a new message handler
+# decoration take 3 arguments:
+#   - exchange name (must be defined)
+#   - queue name (must be defined)
+#   - queue binding key
+# Function but have this prototype: (str, Messahe) -> Any
+#   - body is the payload
+#   - msg is the message object (kombu.transport.myamqp.Message)
+#
+# /!\ only one task per queue
+@CustomConsumer.consumer('test', 'test.test', 'test.test.*')
+def hello(body, msg):
+    print('COUCOU')
+
+
+@CustomConsumer.consumer('test', 'test.hello', 'test.hello.*')
+def world(body, msg):
+    raise RuntimeError('Je viens de planter :(')
+
+
+# build consumers
+# take 1 parameter
+#   - app is the celery app
+CustomConsumer.build(app)
